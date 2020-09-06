e35535dc0b9af7ae6ca11fdd46ae135be4467d4e
David Mitchell <monch1962@gmail.com>
2020-09-06 04:06:30 +0000

    Started creating test

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"net/http"
 	"net/http/httptest"
-	// "testing"
 	"io"
 	"io/ioutil"
 	"fmt"
@@ -11,7 +10,8 @@ import (
 
 func main() {
 	handler := func(w http.ResponseWriter, r *http.Request){
-		io.WriteString(w, "ping")
+		w.Header().Set("Content-Type", "application/json")
+		io.WriteString(w, `{"alive": true}`)
 	}
 	req := httptest.NewRequest("GET", "http://example.com/foo", nil)
 	w := httptest.NewRecorder()
@@ -19,7 +19,9 @@ func main() {
 
 	resp := w.Result()
 	body, _ := ioutil.ReadAll(resp.Body)
+	resp.Body.Close()
 
 	fmt.Println(resp.StatusCode)
+	fmt.Println(resp.Header)
 	fmt.Println(string(body))
 }
